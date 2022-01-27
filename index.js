977a117dd892b0d0cc2c4d7b3db19a85aa2c8b0e
AndrizalChaidar <andrizal.cs@gmail.com>
2022-01-27 14:44:37 +0700

    AndrizalChaidar

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+- SMAN 38 Kampung baru
+- Puskesmas Kelurahan Baru
+- SMAN 88
+ */
+
+const tempatVaksin = {kalisari: {
+    'SDN 05 KALISARI': 10,
+    'GOR KALISARI': 2,
+    'GOR CIRACAS': 1,
+    'PUSKESMAS KALISARI': 5,
+    'SMPN 179 KALISARI ': 3
+},
+pekayon: {
+    'SMPN 184 PEKAYON': 1,
+    'PUSKESMAS PEKAYON': 2,
+    'SMAN 106 JAKARTA': 3,
+    'SDN PEKAYON 09 PAGI': 5,
+},
+cijantung: {
+    'RS KESDAM JAYA CIJANTUNG': 3,
+    'SMP NEGRI 251': 4,
+    'BAYU YUDHA ARENA': 7,
+    'SMP NEGRI 217 JAKARTA': 2,
+    'STKIP KUSUMANEGARA': 1,
+},
+
+}
+
+console.log(Object.keys(tempatVaksin.kalisari))
