78ee4bf480a8c5e07a3cd98ef96a1dd91860a5a3
AndrizalChaidar <andrizal.cs@gmail.com>
2022-01-28 04:06:36 +0700

    update keseluruhan

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const tempatVaksin = {
                 'PUSKESMAS KALISARI',
                 'SMPN 179 KALISARI '
             ],
-            waktu: 'February 5, 2022 07:00:000'
+        waktu: 'February 5, 2022 07:00:000'
     },
     pekayon: {
         lokasi:
@@ -18,7 +18,7 @@ const tempatVaksin = {
                 'SMAN 106 JAKARTA',
                 'SDN PEKAYON 09 PAGI'
             ],
-            waktu: 'February 10, 2022 07:00:000'
+        waktu: 'February 10, 2022 07:00:000'
     },
     cijantung: {
         lokasi:
@@ -29,7 +29,7 @@ const tempatVaksin = {
                 'SMP NEGRI 217 JAKARTA',
                 'STKIP KUSUMANEGARA'
             ],
-            waktu: 'February 7, 2022 07:00:000'
+        waktu: 'February 7, 2022 07:00:000'
     },
     'Kampung Baru': {
         lokasi:
@@ -38,7 +38,7 @@ const tempatVaksin = {
                 'Puskesmas Kelurahan Baru',
                 'SMAN 88'
             ],
-            waktu: 'March 15, 2022 07:00:000'
+        waktu: 'March 15, 2022 07:00:000'
     },
     gedong: {
         lokasi:
@@ -48,20 +48,16 @@ const tempatVaksin = {
                 'KPAD Cijantung',
                 'Gor Bulutangkis Garuda'
             ],
-            waktu: 'March 3, 2022 07:00:000'
+        waktu: 'March 3, 2022 07:00:000'
     }
 }
 
 
-let bambang = {birthDate: '2021-01-07', tempat: { lokasi: 'SDN 05 KALISARI', kelurahan: 'kalisari', waktu: 'February 5, 2022 07:00:000' }}
-let suparman = {birthDate: '2021-01-07', tempat: { lokasi: 'SDN Gedong 12 Pagi', kelurahan: 'gedong', waktu: 'March 3, 2022 07:00:000' }}
+let bambang = { birthDate: '2021-01-07', tempat: { lokasi: 'SDN 05 KALISARI', kelurahan: 'kalisari', waktu: 'February 5, 2022 07:00:000' } }
+let suparman = { birthDate: '2021-01-07', tempat: { lokasi: 'SDN Gedong 12 Pagi', kelurahan: 'gedong', waktu: 'March 3, 2022 07:00:000' } }
 
 localStorage.setItem('Bambang', JSON.stringify(bambang))
 localStorage.setItem('Suparman', JSON.stringify(suparman))
-let local = {...localStorage}
-let waktu = JSON.parse(local['Bambang']).tempat.waktu
-let a =new Date(waktu)
-console.log(a.toLocaleDateString())
 
 let submitBtnEl = document.getElementById("submit-button")
 let closePopUpEl = document.getElementById('popUpBtn')
@@ -77,7 +73,7 @@ submitBtnEl.addEventListener("click", function (event) {
     if (!name) {
         formError[0].classList.add("active")
         return
-    } 
+    }
     formError[0].classList.remove("active")
     if (!birthDate) {
         formError[1].classList.add("active")
@@ -87,25 +83,28 @@ submitBtnEl.addEventListener("click", function (event) {
 
     if (localStorage[name]) {
         let objUser = JSON.parse(localStorage[name]);
-        renderPopUp('index', name, objUser)
+        renderPopUp(name, objUser)
     } else {
-        localStorage.setItem(name, JSON.stringify({birthDate}))
+        localStorage.setItem(name, JSON.stringify({ birthDate }))
+        localStorage.setItem('userName', name)
+        localStorage.setItem('birthDate', birthDate)
         document.location.href = "index2.html"
     }
+
 })
 
-function renderPopUp (page, name, objUser) {
-    if (page === 'index') {
-        const arrInputUser = document.querySelectorAll('.input-popup');
-        arrInputUser[0].innerText = name;
-        const lokasi = objUser.tempat.lokasi;
-        const kelurahan = objUser.tempat.kelurahan;
-        const tanggal = objUser.tempat.waktu
-        vaksinTimeGlobal = new Date(tanggal)
-        arrInputUser[1].innerText = `${lokasi}, Kec. Pasar Rebo, Kel. ${kelurahan[0].toUpperCase() + kelurahan.slice(1)}`;
-        arrInputUser[2].innerText = `${new Date(tanggal).toLocaleDateString()}`
-        updateCountdown(new Date(tanggal));
-    }
+function renderPopUp(name, objUser) {
+
+    const arrInputUser = document.querySelectorAll('.input-popup');
+    arrInputUser[0].innerText = name;
+    const lokasi = objUser.tempat.lokasi;
+    const kelurahan = objUser.tempat.kelurahan;
+    const tanggal = objUser.tempat.waktu
+    vaksinTimeGlobal = new Date(tanggal)
+    arrInputUser[1].innerText = `${lokasi}, Kec. Pasar Rebo, Kel. ${kelurahan[0].toUpperCase() + kelurahan.slice(1)}`;
+    arrInputUser[2].innerText = `${new Date(tanggal).toLocaleDateString()}`
+    updateCountdown(new Date(tanggal));
+
     popUpContainerEl.classList.add('active')
 }
 
@@ -119,34 +118,34 @@ const seconds = document.getElementById('seconds');
 
 // Update countdown time
 function updateCountdown(vaksinTime = vaksinTimeGlobal) {
-	const currentTime = new Date();
-	const diff = vaksinTime - currentTime; // vaksinTime
+    const currentTime = new Date();
+    const diff = vaksinTime - currentTime; // vaksinTime
 
-	const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-	const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-	const m = Math.floor(diff / 1000 / 60) % 60;
-	const s = Math.floor(diff / 1000) % 60;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
 
-	days.innerHTML = d;
-	hours.innerHTML = h < 10 ? '0' + h : h;
-	minutes.innerHTML = m < 10 ? '0' + m : m;
-	seconds.innerHTML = s < 10 ? '0' + s : s;
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
 
     intervalCountDown = setInterval(updateCountdown, 1000)
 }
 
 
 
-closePopUpEl.addEventListener('click', () => {
-    popUpContainerEl.classList.remove('active')
+
+if (closePopUpEl) {
+    closePopUpEl.addEventListener('click', () => {
+        popUpContainerEl.classList.remove('active')
+        clearInterval(intervalCountDown)
+    })
+}
+
+
+window.addEventListener('load', () => {
     clearInterval(intervalCountDown)
 })
 
-// let loadFirst = true
-// window.addEventListener('load', () => {
-//     clearInterval(intervalCountDown)
-//     // if (loadFirst) {
-//     //     loadFirst = false
-//     //     document.location.href = "index.html";
-//     // } 
-// })
